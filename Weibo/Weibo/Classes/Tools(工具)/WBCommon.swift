21b07cfd8eff2971bed7d0d689038f2b2c46f9c3
乔春晓 <>
2019-03-18 15:24:04 +0800

    03-添加应用程序信息

--- Weibo/Weibo/Classes/Tools(工具)/WBCommon.swift
+++ Weibo/Weibo/Classes/Tools(工具)/WBCommon.swift
@@ -8,6 +8,15 @@
 
 import Foundation
 
+// MARK: - 应用程序信息
+// 应用程序 ID
+let WBAppKey = "259310452"
+// 应用h程序加密信息（开发者可以申请修改）
+let WBAppSecret = "0f65fb78704b5d2e8a2d992dca88a4b7"
+// 回调地址 - 登录完成跳转的 URL 参数以GET形式拼接
+let WBRedirectURI = "http://baidu.com"
+
+
 // MARK: - 全局通知定义
 // 用户需要登录通知
 let WBUserShouldLoginNotification = "WBUserShouldLoginNotification"
